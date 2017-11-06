4d6edb5c4c76bfc62b67276fa1afb1719cd5815d
Ken-Ching <ken.bx.cheng.prog@qq.com>
2017-11-06 15:15:03 +0800

    commit

--- common-util/src/main/java/team/kc/util/propertites/PropertiesUtil.java
+++ common-util/src/main/java/team/kc/util/propertites/PropertiesUtil.java
@@ -7,11 +7,23 @@ import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Tool for get value from properties file
+ * @author KenC
+ *
+ */
 public class PropertiesUtil {
-	protected static final Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
+	protected static final Logger logger = 
+			LoggerFactory.getLogger(PropertiesUtil.class);
 	
 	public static final String PROPERTIES_FILE_EXTEND = ".properties";
 	
+	/**
+	 * Get the {@link Properties} from path
+	 * @param path path of the properties file
+	 * @return Return {@link Properties} for the path, 
+	 * if the path is not existed, the {@link Properties} is empty. 
+	 */
 	public static Properties getFromFile (String path) {
 		Properties properties = new Properties();
 		try {
@@ -23,6 +35,12 @@ public class PropertiesUtil {
 		return properties;
 	}
 	
+	/**
+	 * Get the property value from the path for the specified propertyName
+	 * @param path path of the properties file
+	 * @param propertyName specified propertyName
+	 * @return the property value
+	 */
 	public static String getProperty (String path, String propertyName) {
 		Properties properties = getFromFile (path);
 		return properties.getProperty(propertyName);
